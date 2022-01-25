190b9099d1b4b895799db0e3c7beb21d28f4eaed
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-01-25 01:46:34 +0100

    taco's for order loading

--- src/app/components/order/order-list/order-list.component.ts
+++ src/app/components/order/order-list/order-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {TacoOrderService} from "../../../services/taco-order.service";
 import {Observable} from "rxjs";
 import {TacoOrder} from "../../../model/tacoOrder.model";
+import {Taco} from "../../../model/taco.model";
+import {TacoService} from "../../../services/taco.service";
 
 @Component({
   selector: 'app-order-list',
@@ -10,16 +12,22 @@ import {TacoOrder} from "../../../model/tacoOrder.model";
 })
 export class OrderListComponent implements OnInit {
   orders: Observable<TacoOrder[]>;
+  tacos: Taco[] | undefined;
 
-  constructor(private service: TacoOrderService) {
+  constructor(private service: TacoOrderService, private tacoService: TacoService) {
     this.orders = service.getOrders();
   }
 
   ngOnInit(): void {
   }
 
-  onClick(id: Number | undefined) {
+  onClick(id: number | undefined) {
     console.log("Id: " + id);
+    if (id) {
+      this.tacoService.getTacosForOrder(id).subscribe(data => {
+        this.tacos = data;
+      })
+    }
   }
 
 }
