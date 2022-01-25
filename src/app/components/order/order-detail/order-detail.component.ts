8d6685c44de9267d6cd3f242d7c75e0a797d8310
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-01-25 20:28:20 +0100

    navigation from taco to order added

--- src/app/components/order/order-detail/order-detail.component.ts
+++ src/app/components/order/order-detail/order-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-order-detail',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-detail.component.css']
 })
 export class OrderDetailComponent implements OnInit {
+  orderId: number
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.orderId = this.route.snapshot.params.id;
+  }
 
   ngOnInit(): void {
   }
